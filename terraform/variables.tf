beb1d078e1ec4843fe93b71689e598ed866e5534
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2025-08-27 08:55:16 +0100

    OLH-2842: Add a variable for environment name
    We only want to deploy the restore testing stack in staging, so
    Terraform needs to know which environment it's operating in.
    
    I've deliberately only allowed staging, integration and production even
    though we have more environments as these are the only ones which are
    configured here. If we do need to run this Terraform in eg. dev or build
    we can add those environments to the validation at the same time as we
    configure them.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,13 @@
+variable "environment" {
+  type        = string
+  description = "The environment name"
+  validation {
+    condition     = contains(["staging", "integration", "production"], var.environment)
+    error_message = "Valid values for var: environment are (staging, integration, production)"
+  }
+
+}
+
 variable "notifications_slack_channel_id" {
   type        = string
   description = "ID of the channel to send Slack notifications to"
